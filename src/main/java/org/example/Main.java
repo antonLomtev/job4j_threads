3137fefbe8878aa1afd857a76335af999727d820
a.stogniy <a.stogniy@astondevs.ru>
2024-03-24 22:20:39 +0300

    2. Ветки в IDEA [#504828]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("task_124176");
+        System.out.println("task_139840");
     }
 }
