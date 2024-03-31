779c910b9d6b4a82cd83ebe1bcc6c64fc7f5bd77
a.stogniy <a.stogniy@astondevs.ru>
2024-03-31 21:23:14 +0300

    3.4. Временное хранение. git stash. [#504832]stash unstash

--- src/main/java/org/example/stash/Stash.java
+++ src/main/java/org/example/stash/Stash.java
@@ -3,6 +3,6 @@ package org.example.stash;
 public class Stash {
     private String name;
     public void print() {
-        System.out.println("какой-то смысл до");
+        System.out.println("что-то делал до");
     }
 }
